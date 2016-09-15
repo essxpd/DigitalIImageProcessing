be691662beb0fc0d75c18a7b587bbfc32fa1d20c
Evan Schaffer <essxpd@mail.missouri.edu>
2016-09-15 13:02:04 -0500

    Finished histeq

--- Intensity Transformation and Spatial Filtering/IntensityTransformations.m
+++ Intensity Transformation and Spatial Filtering/IntensityTransformations.m
@@ -1,1 +1,22 @@
+clear all
+close all
 addpath('Images');
+
+%% Hist eq on lena_biased
+
+% Load image
+I = imread('lena_biased.jpg');
+figure('Name', 'Histogram Equalizization on Lena_biased.jpg');
+subplot(2,2,1), imshow(I);
+title('Lena\_biased.jpg');
+
+subplot(2,2,3), histogram(I);
+title('Lena\_biased.jpg Histogram');
+
+% Histogram equilzation
+J = my_histeq(I);
+subplot(2,2,2), imshow(J);
+title('Equalized Lena\_biased.jpg');
+
+subplot(2,2,4), histogram(J);
+title('Equalizeed Lena\_biased.jpg Histogram');
---
+++ Intensity Transformation and Spatial Filtering/my_histeq.m
@@ -0,0 +1,17 @@
+function [ output ] = my_histeq( input )
+%my_histeq equalizes the histogram of the input image and maps it to an
+%output image
+
+    % Create a histogram from 0 to 255 to count the number of occurances of
+    % each pixel intensity
+    maxIntensity = 255;
+    h = hist(input(:),0:maxIntensity);
+    
+    % Cumulative probability 
+    cdf = cumsum(h / size(input(:),1));
+    
+    % Map input to output 
+    output = cdf(input + 1) * maxIntensity;
+    output = uint8(output);
+end
+
